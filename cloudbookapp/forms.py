6381eec4cad48edc34128dc9066cd70a5754285f
mejkej <krus3@icloud.com>
2023-05-23 06:45:58 +0000

    added custom user creation and authentication to forms.py and changed the min length in setting.py

---
+++ cloudbookapp/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=20,
+        min_length=3,
+        widget=forms.TextInput(attrs={'placeholder': 'Username'}),
+    )
+
+    password1 = forms.CharField(
+        max_length=20,
+        min_length=5,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),
+    )
+
+    password2 = forms.CharField(
+        max_length=20,
+        min_length=5,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Comfirm Password'}),
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2")
+        
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Username'}),
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),
+    )
