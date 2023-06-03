c9221f646bfb916e3b89adee61335b2bc698d5b7
mejkej <krus3@icloud.com>
2023-06-04 04:44:48 +0700

    added exit btn for edit.html to

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,15 @@
-const closeFormBtn = document.getElementById('closenewnote');
+const exitNewFormBtn = document.getElementById('exitnewnote');
+const exitEditFormBtn = document.getElementById('exiteditnote');
 
-closeFormBtn.addEventListener('click', (exit) => {
+exitNewFormBtn.addEventListener('click', (exit) => {
     const confirmExit = window.confirm("There are unsaved changes are you sure you want to exit?");
+    if (confirmExit == true) {
+        window.location.href = '/';
+    }
+ });
+
+ exitEditFormBtn.addEventListener('click', (exit) => {
+    const confirmExit = window.confirm("There might be unsaved changes, are you sure you want to exit?");
     if (confirmExit == true) {
         window.location.href = '/';
     }
